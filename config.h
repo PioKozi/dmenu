89ab0727964184dd83a3d40ae939aa5446956b6e
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-07-26 21:17:47 +0100

    using better colours

--- config.h
+++ config.h
@@ -2,23 +2,22 @@
 /* Default settings; can be overriden by command line. */
 
 static int instant = 0;
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;
+static int topbar  = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy   = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"Jetbrains Mono Nerd Font:size=11"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"Jetbrains Mono Nerd Font:size=11"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*                        fg         bg       */
-	[SchemeNorm]          = { "#ebdbb2", "#1d2021" },
-	[SchemeSel]           = { "#cc241d", "#1d2021" },
-    [SchemeSelHighlight]  = { "#cc241d", "#689d6a" },
-    [SchemeNormHighlight] = { "#ebdbb2", "#689d6a" },
-    [SchemeOut]           = { "#1d2021", "#458588" },
+    /*                        fg         bg       */
+    [SchemeNorm]          = {"#ebdbb2", "#1d2021"},
+    [SchemeSel]           = {"#cc241d", "#1d2021"},
+    [SchemeSelHighlight]  = {"#cc241d", "#665c54"},
+    [SchemeNormHighlight] = {"#ebdbb2", "#665c54"},
+    [SchemeOut]           = {"#1d2021", "#458588"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
