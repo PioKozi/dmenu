0bd3ed7c36e124eb222783705cd88fae5c9ed3dc
piokozi <47647338+PioKozi@users.noreply.github.com>
2020-07-26 20:17:04 +0100

    fuzzy finding highlighting patch

--- config.h
+++ config.h
@@ -10,10 +10,12 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*                fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#1d2021" },
-	[SchemeSel]  = { "#cc241d", "#1d2021" },
-	[SchemeOut]  = { "#000000", "#83a598" }, // I don't actually know when this is used, probably not using dmenu to the fullest
+	/*                        fg         bg       */
+	[SchemeNorm]          = { "#ebdbb2", "#1d2021" },
+	[SchemeSel]           = { "#cc241d", "#1d2021" },
+    [SchemeSelHighlight]  = { "#cc241d", "#689d6a" },
+    [SchemeNormHighlight] = { "#ebdbb2", "#689d6a" },
+    [SchemeOut]           = { "#1d2021", "#458588" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
